5e498a67cfded33359dd73c8138a5e32b89d211b
Your Name <you@example.com>
2021-12-29 17:07:15 +0000

    Fix tabs

--- src/FrontendFrameworkTodoTestLit.js
+++ src/FrontendFrameworkTodoTestLit.js
@@ -4,30 +4,31 @@ import { ListDisplay } from "./list-display.js";
 
 export class FrontendFrameworkTodoTestLit extends LitElement {
 
-  static get styles() {
-    return css`
+    static get styles() 
+    {
+        return css`
 
-      main {
-        flex-grow: 1;
-      }
+            main {
+            flex-grow: 1;
+            }
+        `;
+    }
 
-    `;
-  }
+    render() 
+    {
+        return html`
+            <main>
+                <h1>ToDo using Lit</h1>
+                <list-display title="Things To Do:"></list-display>
 
-  render() {
-    return html`
-      <main>
-        <h1>ToDo using Lit</h1>
-        <list-display title="Things To Do:"></list-display>
-
-        <form @submit="${this.submit_todo}">
-            <label for="add-todo">Add Todo:</label> 
-            <input type="text" id="add-todo" name="add-todo"><br><br>
-            <input type="submit" value="Submit">
-        </form>
-      </main>
-    `;
-  }
+                <form @submit="${this.submit_todo}">
+                    <label for="add-todo">Add Todo:</label> 
+                    <input type="text" id="add-todo" name="add-todo"><br><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </main>
+        `;
+    }
 
     submit_todo(e)
     {
