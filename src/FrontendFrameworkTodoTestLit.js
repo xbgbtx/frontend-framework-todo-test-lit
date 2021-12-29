bc90b1f34c6638c65de1ec182c44af5053028952
Your Name <you@example.com>
2021-12-29 16:57:13 +0000

    Form submit handler

--- src/FrontendFrameworkTodoTestLit.js
+++ src/FrontendFrameworkTodoTestLit.js
@@ -19,8 +19,20 @@ export class FrontendFrameworkTodoTestLit extends LitElement {
       <main>
         <h1>ToDo using Lit</h1>
         <list-display title="Things To Do:"></list-display>
+
+        <form @submit="${this.submit_todo}">
+            <label for="add-todo">Add Todo:</label> 
+            <input type="text" id="add-todo" name="add-todo"><br><br>
+            <input type="submit" value="Submit">
+        </form>
       </main>
     `;
   }
+
+    submit_todo(e)
+    {
+        e.preventDefault();
+        console.log("Submitted todo");
+    }
 }
 
