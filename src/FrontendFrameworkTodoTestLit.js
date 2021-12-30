991eb13c8a651e630adc553f5bd19864c26578f2
Your Name <you@example.com>
2021-12-30 14:48:47 +0000

    Reset project
    Safe in the knowledge that I now know.

--- src/FrontendFrameworkTodoTestLit.js
+++ src/FrontendFrameworkTodoTestLit.js
@@ -29,6 +29,7 @@ export class FrontendFrameworkTodoTestLit extends LitElement {
         super ();
         this.todo=[];
         this.addEventListener('done', e => console.log(e));
+        this.item_count = 0;
     }
 
     render() 
@@ -36,41 +37,8 @@ export class FrontendFrameworkTodoTestLit extends LitElement {
         return html`
             <main>
                 <h1>ToDo using Lit</h1>
-                <list-display 
-                    title="Things To Do:"
-                    items="${JSON.stringify(this.todo)}">
-                </list-display>
-
-                <form @submit="${this.submit_todo}">
-                    <label for="add-todo">Add Todo:</label> 
-                    <input type="text" id="add-todo-input" name="add-todo"><br><br>
-                    <input type="submit" value="Submit">
-                </form>
             </main>
         `;
-    }
-
-    submit_todo(e)
-    {
-        e.preventDefault();
-        console.log("Submitted todo");
-        let idx = this.todo.length + 1;
-        let id = `todo-${idx}`;
-        let item = html`
-            <list-item 
-                id=${id}
-                item_idx=${idx}
-                item_text='todo-text go here'
-                click_event_name='done'
-            ></list-item>
-        `;
-        this.todo = [...this.todo, item];
-    }
-
-    mark_done(e)
-    {
-        console.log("Marked done");
-        console.log(e);
     }
 }
 
